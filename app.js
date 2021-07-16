00aa3b447fc4753ed35c9b1e18465cad94e1a20b
Barbara Bateli <barbarabateli@MacBook-Air-de-Barbara.local>
2021-07-16 00:55:05 -0300

    done

--- app.js
+++ app.js
@@ -14,86 +14,100 @@ app.get("/pokemon", (req, res) => {
   });
   
   // search pokemon
-  app.get("/pokemon/search", (req, res) => {
-    const queryParams = req.query;
-    console.log(queryParams);
-  
-    for (let key in queryParams) {
-      const foundPokemon = allPokemon.find((pokemonElement) => {
-        return pokemonElement[key]
-          .toLowerCase()
-          .includes(queryParams[key].toLowerCase());
-      });
-  
-      if (foundPokemon) {
-        return res.json(foundPokemon);
-      } else {
-        return res.json({ msg: "Pokemon not found!" });
-      }
-    }
-  
-    res.json(queryParams);
-  });
-  
-  // especific pokemon
   app.get("/pokemon/:id", (req, res) => {
     const id = req.params.id;
+    
+      const foundPokemon = allPokemon.find((currentPokemon) => {
+        return currentPokemon.id === Number(id);
+      });
+       
+      if (foundPokemon) {
+        return res.json(foundPokemon);
+      }
+        return res.json({ msg: "Pokemon not found!" });
+      });
   
-    const foundPokemon = allPokemon.find((pokemonUnique) => {
-      return pokemonUnique.id === Number(id);
+  // especific pokemon
+  app.get("/search", (req, res) => {
+    for (let key in req.query) {
+      const filteredPokemon = allPokemon.filter((currentPokemon) => {
+        // Pesquisando por tipos
+        if (key === "types") {
+          return currentPokemon.types.includes(req.query.types);
+        }
+
+        return currentPokemon.name
+        .toLowerCase()
+        .includes(req.query.name.toLowerCase());
     });
-  
-    if (foundPokemon) {
-      return res.json(foundPokemon);
-    } else {
-      return res.json({ msg: "Pokemon not found!" });
+
+    if (filteredPokemon.length) {
+      return res.json(filteredPokemon);
     }
+
+    return res.json({ msg: "No Pokemon matches this search." });
+  }
+});
+
+app.post("/pokemon", (req, res) => {
+  const formData = req.body;
+
+  // Pegando o último id da lista de todos os Pokemons
+  const lastId = allPokemon[allPokemon.length - 1].id;
+
+  // O novo Pokemon vai continuar a sequencia de ids
+  const newPokemon = { ...formData, id: lastId + 1 };
+
+  allPokemon.push(newPokemon);
+
+  return res.json(newPokemon);
+});
+    
+app.put("/pokemon/:id", (req, res) => {
+  const formData = req.body;
+
+  // Encontrar o Pokemon com o id do parametro de rota
+  const id = req.params.id;
+
+  const foundPokemon = allPokemon.find((currentPokemon) => {
+    return currentPokemon.id === Number(id);
   });
-  
-  // create
-  app.post("/pokemon", (req, res) => {
-    const formData = req.body;
-  
-    const newPokemon = {
-      id: uuidv4(),
-      name: formData.name,
-      types: formData.types,
-      height: formData.height,
-      weight: formData.weight,
-      sprite: formData.sprite,
-    };
-  
-    allPokemon.push(newPokemon);
-  
-    return res.json(newPokemon);
-  });
-  
-  // update
-  app.put("/pokemon/:id", (req, res) => {
-    const formData = req.body;
-  
-    const id = req.params.id;
-  
-    const foundPokemon = allPokemon.find((pokemonElement) => {
-      return pokemonElement.id === id;
+
+  if (foundPokemon) {
+    // Atualiza o elemento da array com os dados do corpo (body) da requisição
+    const index = allPokemon.findIndex((currentPokemon) => {
+      return currentPokemon.id === Number(id);
     });
-  
-    const index = allPokemon.indexOf(foundPokemon);
-    allPokemon[index] = { ...foundPokemon, ...formData };
-    return res.json(allPokemon[index]);
-  });
-  
-  // delete
-  app.delete("/pokemon/:id", (req, res) => {
-    const index = allPokemon.findIndex((pokemonElement) => {
-      return pokemonElement.id === req.params.id;
-    });
-  
-    if (index > 0) {
-      allPokemon.splice(index, 1);
-      return res.json({ msg: "Contact deleted successfully" });
+
+    if (index > -1) {
+      allPokemon[index] = { ...foundPokemon, ...formData };
+
+      return res.json(allPokemon[index]);
     } else {
-      return res.json({ msg: "Contact not found." });
+      return res.json({ msg: "Pokemon not found." });
     }
+  }
+
+  return res.json({ msg: "Pokemon not found." });
+});
+
+// 6. A DELETE /pokemon/:id route, that deletes an existing Pokemon and returns a success message
+
+app.delete("/pokemon/:id", (req, res) => {
+  const id = req.params.id;
+
+  const index = allPokemon.findIndex((currentPokemon) => {
+    return currentPokemon.id === Number(id);
   });
+
+  if (index > -1) {
+    allPokemon.splice(index, 1);
+    return res.json({ msg: "Pokemon successfully removed." });
+  }
+
+  return res.json({ msg: "Pokemon not found." });
+});
+
 app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
+
+   
